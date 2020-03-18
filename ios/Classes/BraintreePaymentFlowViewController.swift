56f62300f7b724dd7624cd636c87fd6e471044fd
ScruffyHipster <tomsmiled@me.com>
2020-03-18 16:42:49 +0000

    Added braintree flow controller

---
+++ ios/Classes/BraintreePaymentFlowViewController.swift
@@ -0,0 +1,53 @@
+//
+//  BraintreePaymentFlowViewController.swift
+//  flutter_braintree
+//
+//  Created by Thomas Murray on 18/03/2020.
+//
+
+import UIKit
+import Braintree
+
+class BraintreePaymentFlowViewController: UIViewController, BTViewControllerPresentingDelegate {
+
+    func paymentDriver(_ driver: Any, requestsPresentationOf viewController: UIViewController) {
+        viewController.present(viewController, animated: true, completion: nil)
+    }
+
+    func paymentDriver(_ driver: Any, requestsDismissalOf viewController: UIViewController) {
+        viewController.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   // This allows you to initialise your custom UIViewController without a nib or bundle.
+    convenience init() {
+        self.init(nibName:nil, bundle:nil)
+    }
+
+    // This extends the superclass.
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        print("*** loaded")
+    }
+
+    // This is also necessary when extending the superclass.
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented") // or see Roman Sausarnes's answer
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ios/Classes/SwiftFlutterBraintreePlugin.swift
+++ ios/Classes/SwiftFlutterBraintreePlugin.swift
@@ -4,65 +4,68 @@ import Braintree
 import BraintreeDropIn
 
 public class SwiftFlutterBraintreePlugin: NSObject, FlutterPlugin {
-    
+
     var isHandlingResult: Bool = false
-    
-    
+    private lazy var btPaymentViewController: BraintreePaymentFlowViewController = {
+        return BraintreePaymentFlowViewController()
+    }()
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_braintree.drop_in", binaryMessenger: registrar.messenger())
         let customChannel = FlutterMethodChannel(name: "flutter_braintree.custom", binaryMessenger: registrar.messenger())
-        
+
         let instance = SwiftFlutterBraintreePlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
         registrar.addMethodCallDelegate(instance, channel: customChannel)
     }
 
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if call.method == "start" {
             guard !isHandlingResult else { result(FlutterError(code: "drop_in_already_running", message: "Cannot launch another Drop-in activity while one is already running.", details: nil)); return }
-            
+
             isHandlingResult = true
-            
+
             let dropInRequest = BTDropInRequest()
-            
+
             if let amount = string(for: "amount", in: call) {
                 dropInRequest.threeDSecureRequest?.amount = NSDecimalNumber(string: amount)
             }
-            
+
             if let requestThreeDSecureVerification = bool(for: "requestThreeDSecureVerification", in: call) {
                 dropInRequest.threeDSecureVerification = requestThreeDSecureVerification
             }
-            
+
             if let vaultManagerEnabled = bool(for: "vaultManagerEnabled", in: call) {
                 dropInRequest.vaultManager = vaultManagerEnabled
             }
 
             let clientToken = string(for: "clientToken", in: call)
             let tokenizationKey = string(for: "tokenizationKey", in: call)
-            
+
             guard let authorization = clientToken ?? tokenizationKey else {
                 result(FlutterError(code: "braintree_error", message: "Authorization not specified (no clientToken or tokenizationKey)", details: nil))
                 isHandlingResult = false
                 return
             }
-            
+
             let dropInController = BTDropInController(authorization: authorization, request: dropInRequest) { (controller, braintreeResult, error) in
                 controller.dismiss(animated: true, completion: nil)
-                
+
                 self.handle(braintreeResult: braintreeResult, error: error, flutterResult: result)
                 self.isHandlingResult = false
             }
-            
+
             guard let existingDropInController = dropInController else {
                 result(FlutterError(code: "braintree_error", message: "BTDropInController not initialized (no API key or request specified?)", details: nil))
                 isHandlingResult = false
                 return
             }
-                
+
             UIApplication.shared.keyWindow?.rootViewController?.present(existingDropInController, animated: true, completion: nil)
         }
-        else if call.method == "tokenizeCreditCard" {
+        else if call.method == "request3dsNonce" {
+
             let braintreeClient = BTAPIClient(authorization: string(for: "authorization", in: call)!)
             let cardClient = BTCardClient(apiClient: braintreeClient!)
             let card = BTCard(
@@ -71,7 +74,7 @@ public class SwiftFlutterBraintreePlugin: NSObject, FlutterPlugin {
                 expirationYear: string(for: "expirationYear", in: call)!,
                 cvv: string(for: "cvv", in: call)!
             )
-            cardClient.tokenizeCard(card) { (card, error) in 
+            cardClient.tokenizeCard(card) { (card, error) in
                 if card != nil {
                     let pm: [String : Any] = [
                         "nonce": card!.nonce,
@@ -85,12 +88,47 @@ public class SwiftFlutterBraintreePlugin: NSObject, FlutterPlugin {
                 }
             }
         }
+
+        else if call.method == "tokenizeCreditCard" {
+
+            let authToken = string(for: "authorization", in: call)
+            let amount =  Double(string(for: "amount", in: call) ?? "0.1")
+            let email =  string(for: "email", in: call)
+            let nonce =  string(for: "nonce", in: call)
+
+            let braintreeApi = BTAPIClient(authorization: authToken ?? "")
+            let flowRequest = BTPaymentFlowDriver(apiClient: braintreeApi!)
+            flowRequest.viewControllerPresentingDelegate = self.btPaymentViewController
+
+            let threeDSecureRequest = BTThreeDSecureRequest()
+            threeDSecureRequest.amount = NSDecimalNumber(value: amount ?? 0.1)
+            threeDSecureRequest.email = email ?? ""
+            threeDSecureRequest.nonce = nonce ?? ""
+            threeDSecureRequest.threeDSecureRequestDelegate = self
+
+            flowRequest.startPaymentFlow(threeDSecureRequest) { (threeDSecureresult, error) in
+                guard let secureResult = threeDSecureresult as? BTThreeDSecureResult, let tokenizedCard = secureResult.tokenizedCard else {
+                    result(FlutterError(code: "braintree_error", message: "error occured when creating payment flow \(error?.localizedDescription)", details: nil))
+                    return
+                }
+                if tokenizedCard.threeDSecureInfo.liabilityShiftPossible {
+                        if tokenizedCard.threeDSecureInfo.liabilityShifted {
+                            print("*** success from paymentflow")
+                            result(tokenizedCard.nonce)
+                        } else {
+                            result(FlutterError(code: "braintree_error", message: "*** failed during payment flow", details: nil))
+                        }
+                    } else {
+                        result(FlutterError(code: "braintree_error", message: "*** was not possible to authenticate", details: nil))
+                    }
+                }
+            }
+
         else {
             result(FlutterMethodNotImplemented)
         }
     }
-    
-    
+
     private func handle(braintreeResult: BTDropInResult?, error: Error?, flutterResult: FlutterResult) {
         if error != nil {
             flutterResult(FlutterError(code: "braintree_error", message: error?.localizedDescription, details: nil))
@@ -103,21 +141,29 @@ public class SwiftFlutterBraintreePlugin: NSObject, FlutterPlugin {
                                                    "typeLabel": braintreeResult.paymentMethod?.type,
                                                    "description": braintreeResult.paymentMethod?.localizedDescription,
                                                    "isDefault": braintreeResult.paymentMethod?.isDefault]
-            
+
             let resultDict: [String: Any?] = ["paymentMethodNonce": nonceResultDict]
-            
+
             flutterResult(resultDict)
         }
     }
-    
+
 
     private func string(for key: String, in call: FlutterMethodCall) -> String? {
         return (call.arguments as? [String: Any])?[key] as? String
     }
-    
-    
+
+
     private func bool(for key: String, in call: FlutterMethodCall) -> Bool? {
         return (call.arguments as? [String: Any])?[key] as? Bool
     }
-    
+
+}
+
+extension SwiftFlutterBraintreePlugin: BTThreeDSecureRequestDelegate  {
+
+    public func onLookupComplete(_ request: BTThreeDSecureRequest, result: BTThreeDSecureLookup, next: @escaping () -> Void) {
+        next()
+    }
+
 }
